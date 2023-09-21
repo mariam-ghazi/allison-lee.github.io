25f3df894c847ae91a2b68c3758da00269605fbf
Mary Ghazi <THEMAIN@Marys-MacBook-Air.local>
2023-09-21 15:44:17 -0400

    First commit: HTML: - Home page is missing arrow icon - About me page needs to be updated with the admins about me paragraph, pictures, is missing a cursive name icon - Works page is missing project info, names etc. CSS: - Styling of the works Section is not complete. - The start page eventually will include animations Javascript: - Has a copy function that is yet to be tested. - Still need to create the popup feature

---
+++ allison-portfolio.js
@@ -0,0 +1,18 @@
+/*copy email to clip board*/
+/*make "lee" appear AFTER opening webpage*/
+function copy(){
+    var text = document.getElementById("");
+    
+    text.select();
+    navigator.clipbaord.writeText(); 
+
+}
+
+function popup () {
+
+}
+
+
+
+/* or make works thing a scroll bar and have it scrll horzontally?*/
+/*RESUME: write about product engineering for ESP*/
