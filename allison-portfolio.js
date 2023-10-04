74296dd102aca72856de9f0474282fb26adf1ab0
Mary Ghazi <THEMAIN@Marys-MacBook-Air.local>
2023-10-04 02:46:28 -0400

    Fixed the copy email button. Filled formatting and added styling to the works boxes. Started on popup: Overlay created when clicking any of the popup buttons. Added a pink box which will display the extra information about the work. Still need to add icons.

--- allison-portfolio.js
+++ allison-portfolio.js
@@ -1,18 +1,40 @@
 /*copy email to clip board*/
-/*make "lee" appear AFTER opening webpage*/
-function copy(){
-    var text = document.getElementById("");
-    
-    text.select();
-    navigator.clipbaord.writeText(); 
+async function copyContent(text) {
+    /*error handling*/
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err); /*display details of the error*/
+    }
+  }
+  /*Copy text with email id*/
+  /* added DOMContentLoaded event to ensure HTML DOM is loaded first*/
+  document.addEventListener("DOMContentLoaded", function () {
+    /* added click event to the element with id "email"*/
+    document.getElementById('email').addEventListener('click', function () {
+      var copyText = document.getElementById('email').textContent;
+      copyContent(copyText); /*initiate copying text when email element is clicked*/
+    });
+  });
+  
 
-}
-
-function popup () {
-
-}
+/*open pop-up on click*/
+/* added DOMContentLoaded event to ensure HTML DOM is loaded first*/
+/*without this, events wont work */
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const worksPage = document.querySelector(".works"), 
+          expandBox = document.querySelectorAll(".expand-box"),
+          old = document.querySelector(".box-overlay"),
+          overlay = document.querySelector(".pop-up");
 
-/* or make works thing a scroll bar and have it scrll horzontally?*/
-/*RESUME: write about product engineering for ESP*/
+    /*clicking any of the expandBox elements opens a popup*/
+    expandBox.forEach(function(expandBox) {
+        expandBox.addEventListener("click", () => {
+            overlay.classList.add("active");
+        });
+    });
+});
+
